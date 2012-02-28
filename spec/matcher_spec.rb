b932bfefab3a254075f1f28f98f27601e79421b6
Thomas Rix <trix@zappos.com>
2012-02-27 17:38:57 -0800

    spec rename

--- spec/matcher_spec.rb
+++ spec/matcher_spec.rb
@@ -1,6 +1,6 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe Sitemod::Matcher, "#is_true" do
+describe Sitemod::Matcher do
   it "should be true" do
     Sitemod::Matcher.new.is_true?.should be_true
   end
