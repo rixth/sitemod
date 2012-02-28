20aedbe175b1ed1aaff48010a389389eee436697
Thomas Rix <trix@zappos.com>
2012-02-27 23:04:58 -0800

    A tested matcher

--- lib/sitemod/matcher.rb
+++ lib/sitemod/matcher.rb
@@ -1,6 +1,35 @@
+require 'uri'
+
 module Sitemod
   class Matcher
-    def is_true?
+    attr_reader :directories
+
+    def initialize(directories)
+      @directories = directories
+    end
+
+    def get_directories_for_url(url)
+      uri = URI(url)
+      directories.select { |directory| directory_matches_uri?(directory, uri) }
+    end
+
+    def directory_matches_uri?(directory, uri)
+      uri = uri.dup
+
+      # Remove any www from host
+      uri.host.sub!(/^www\./, '')
+
+      # Check for host mis-match (accounting for potential www)
+      return false unless directory[/^(www\.)?#{Regexp.escape(uri.host)}($|\/)/]
+
+      # Check paths, accounting for wildcards, and trailing wildcards
+      directory_path = [*directory.sub(/^.+?($|\/)/, '').split('/')]
+      uri_path = [*uri.path.gsub(/^\/|\/$/, '').split('/')]
+      return false unless directory_path.zip(uri_path).all? do |bits|
+        bits[0] == '*' || bits[0] == bits[1] || (directory_path.last == '*' && bits[1] != nil)
+      end
+
+      # If it hasn't bailed by now, it must be good, right?
       true
     end
   end
--- spec/matcher_spec.rb
+++ spec/matcher_spec.rb
@@ -1,7 +1,106 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-describe Sitemod::Matcher do
-  it "should be true" do
-    Sitemod::Matcher.new.is_true?.should be_true
+describe Sitemod::Matcher, "#directory_matches_uri?" do
+  before(:each) do
+    @matcher = Sitemod::Matcher.new([])
+  end
+
+  it 'matches domains correctly' do
+    @matcher.directory_matches_uri?("example.com", URI("http://example.com")).should be_true
+    @matcher.directory_matches_uri?("example.com", URI("http://www.example.com/with/path")).should be_true
+    @matcher.directory_matches_uri?("example.com", URI("http://different.com")).should be_false
+  end
+
+  it 'matches subdomains correctly' do
+    @matcher.directory_matches_uri?("subdomain.example.com", URI("http://example.com")).should be_false
+    @matcher.directory_matches_uri?("subdomain.example.com", URI("http://subdomain.example.com")).should be_true
+    @matcher.directory_matches_uri?("example.com", URI("http://subdomain.example.com")).should be_false
+  end
+
+  it 'matches paths correctly' do
+    @matcher.directory_matches_uri?("example.com/with_path", URI("http://example.com")).should be_false
+    @matcher.directory_matches_uri?("example.com/with_path", URI("http://example.com/with_path")).should be_true
+    @matcher.directory_matches_uri?("example.com/with_path", URI("http://example.com/with_path/")).should be_true
+    @matcher.directory_matches_uri?("example.com", URI("http://www.example.com/with/path")).should be_true
+  end
+
+  it 'matches nested paths correctly' do
+    @matcher.directory_matches_uri?("example.com/with/nested/path", URI("http://example.com")).should be_false
+    @matcher.directory_matches_uri?("example.com/with/nested/path", URI("http://example.com/with/nested/path")).should be_true
+  end
+
+  it 'matches wildcard paths correctly' do
+    @matcher.directory_matches_uri?("example.com/*/nested/path", URI("http://example.com")).should be_false
+    @matcher.directory_matches_uri?("example.com/*/nested/path", URI("http://example.com/hello/nested")).should be_false
+    @matcher.directory_matches_uri?("example.com/*/nested/path", URI("http://example.com/hello/nested/path")).should be_true
+    @matcher.directory_matches_uri?("example.com/*/nested/path", URI("http://example.com/hello/crazy/path")).should be_false
+    @matcher.directory_matches_uri?("example.com/with/*", URI("http://example.com")).should be_false
+    @matcher.directory_matches_uri?("example.com/*/nested/path", URI("http://example.com/with/nested/path")).should be_true
+    @matcher.directory_matches_uri?("example.com/*/*/path", URI("http://example.com/with/nested/path")).should be_true
+  end
+end
+
+
+describe Sitemod::Matcher, "#get_directories_for_url" do
+  directories = [
+    "www.example.com",
+    "example.com",
+    "subdomain.example.com",
+    "example.com/with_path",
+    "subdomain.example.com/with_path",
+    "example.com/with/nested/path",
+    "example.com/with/wild/*/path",
+    "example.com/with/wild/something/path",
+    "subdomain.example.com/with/nested/path",
+    "subdomain.example.com/a_*/path",
+    "example.net/path/*",
+    "example.net/path/concrete",
+  ]
+
+  before(:each) do
+    @matcher = Sitemod::Matcher.new(directories)
+  end
+
+  context "with domain" do
+    it "correctly matches domains" do
+      @matcher.get_directories_for_url("http://www.example.com").should =~ ["example.com", "www.example.com"]
+    end
+
+    it "correctly matches paths" do
+      @matcher.get_directories_for_url("http://www.example.com/with_path").should =~ [
+        "example.com/with_path",
+        "example.com",
+        "www.example.com"
+      ]
+    end
+
+    it "correctly matches nested paths" do
+      @matcher.get_directories_for_url("http://www.example.com/with/nested/path").should =~ [
+        "example.com/with/nested/path",
+        "example.com",
+        "www.example.com"
+      ]
+    end
+
+    it "correctly matches wildcard paths" do
+      @matcher.get_directories_for_url("http://www.example.com/with/wild/#{rand.to_s}/path").should =~ [
+        "example.com/with/wild/*/path",
+        "example.com",
+        "www.example.com"
+      ]
+
+      # Make sure it matches properly when there is a concrete AND wild match
+      @matcher.get_directories_for_url("http://www.example.com/with/wild/something/path").should =~ [
+        "example.com/with/wild/*/path",
+        "example.com/with/wild/something/path",
+        "example.com",
+        "www.example.com"
+      ]
+
+      # And that it works with trailing wildcards
+      @matcher.get_directories_for_url("http://www.example.net/path/with/randomness/#{rand.to_s}").should =~ [
+        "example.net/path/*"
+      ]
+    end
   end
 end
